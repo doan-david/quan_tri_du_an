1f584fec859a5d55d4edf044e65a964db9e566ab
Hoang <caohoang5969@gmail.com>
2023-05-21 15:56:28 +0700

    upload

---
+++ pages/maincontent/dangky.php
@@ -0,0 +1,30 @@
+
+<p>Đăng ký thành viên</p>
+<form action="" method="POST">
+    <table class="dangky" border="1">
+        <tr>
+            <td>Họ và tên</td>
+            <td><input type="text" size="50" name="hovaten" value=""></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td><input type="text" size="50" name="email" value=""></td>
+        </tr>
+        <tr>
+            <td>Điện thoại</td>
+            <td><input type="text" size="50" name="dienthoai" value=""></td>
+        </tr>
+        <tr>
+            <td>Địa chỉ</td>
+            <td><input type="text" size="50" name="diachi" value=""></td>
+        </tr>
+        <tr>
+            <td>Mật khẩu</td>
+            <td><input type="text" size="50" name="matkhau" value=""></td>
+        </tr>
+        <tr>
+            <td><input style type="submit" name="dangky" value="Đăng ký"></td>
+            <td> <a href="index.php?quanly=dangnhap"> Đăng nhập nếu bạn đã có tài khoản</a></td>
+        </tr>
+    </table>
+</form>
---
+++ pages/maincontent/dangnhap.php
@@ -0,0 +1,22 @@
+
+<form action="" method="POST" autocomplete="off">
+            
+        
+    <table border="1" width="50%" class="" style="text-align: center;border-collapse: collapse;">
+        <tr>
+            <td colspan="2"><h3>Đăng nhập khách hàng</h3></td>
+        </tr>
+        <tr>
+            <td>Tài khoản</td>
+            <td><input type="text" size="50" name="email" value="" placeholder="email..."></td>
+        </tr>
+        <tr>
+            <td>Mật khẩu</td>
+            <td><input type="password" size="50" name="password" value="" placeholder="mật khẩu..."></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" name="dangnhap" value="Đăng nhập"></td>
+            
+        </tr>
+    </table>
+</form>
