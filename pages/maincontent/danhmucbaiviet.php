e8605d00dacba0271fd38f4da46e429fb8b9e7ff
Hoang <caohoang5969@gmail.com>
2023-05-23 11:25:44 +0700

    danhmucbaiviet

---
+++ pages/maincontent/danhmucbaiviet.php
@@ -0,0 +1,46 @@
+<h3>Danh mục bài viết:<span style="text-align: center;text-transform:uppercase;"></span></h3>
+            <div class="row">
+            <ul class="list_baiviet">
+               
+                <div class="col-md-3">
+                <li>
+                    <a href="index.php?quanly=baiviet&id=">
+                    <img class="img img-responsive" width="100%" src="admincp/modules/qlbaiviet/uploads/">
+                    <p style="word-wrap: break-word;" class="title_product">Tên bài viết: <</p>
+                    </a>
+                    <p class="title_product">Tóm tắt:</p>
+                </li>
+                </div>
+                
+            </ul>
+            <div style="clear:both"></div>
+            <style type="text/css">
+                ul.list_trang {
+                    margin: 0;
+                    padding: 0;
+                    list-style: none;
+                }
+                ul.list_trang li {
+                    float: left;
+                    padding: 5px 13px;
+                    margin: 5px;
+                    background: burlywood;
+                    display: block;
+                }
+                ul.list_trang li a{
+                    color: black;
+                    text-align: center;
+                    text-decoration: none;
+                }
+            </style>
+  
+            <p>trang hiện tại:/</p>
+            <ul class="list_trang">
+              
+                <li >
+                    <a href="index.php?quanly=danhmucbaiviet&id=&trang="></a>
+                </li>
+                
+            
+            </ul>
+            </div>
