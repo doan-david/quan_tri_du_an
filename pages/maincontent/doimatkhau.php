cf29da7cfd25cec5bc01c9b7c46ad47cfea833d5
Hoang <caohoang5969@gmail.com>
2023-05-24 11:59:46 +0700

    doimatkhau

---
+++ pages/maincontent/doimatkhau.php
@@ -0,0 +1,26 @@
+
+<p>Đổi mật khẩu</p>
+<form action="" method="POST" autocomplete="off">
+            
+        
+            <table border="1" class="table_login">
+                <tr>
+                    <td colspan="2"><h3>Đổi mật khẩu khách hàng</h3></td>
+                </tr>
+                <tr>
+                    <td>Tài khoản</td>
+                    <td><input type="text" name="email" value=""></td>
+                </tr>
+                <tr>
+                    <td>Mật khẩu cũ</td>
+                    <td><input type="text" name="password_cu" value=""></td>
+                </tr>
+                <tr>
+                    <td>Mật khẩu mới</td>
+                    <td><input type="text" name="password_moi" value=""></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" name="doimatkhau" value="Đổi mật khẩu"></td>
+                </tr>
+            </table>
+        </form>
