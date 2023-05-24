2bc8d21015317f5205bdc5e36267d4750ce5f656
Hoang <caohoang5969@gmail.com>
2023-05-24 12:17:44 +0700

    baiviet

---
+++ pages/maincontent/baiviet.php
@@ -0,0 +1,14 @@
+
+<h3>Bài viết:<span style="text-align: center;text-transform:uppercase;"></span></h3>
+<ul class="baiviet">
+    
+    <li>
+        <h1 style="text-align:center; color: #20784d"></h1>
+        <!-- <img src="admincp/modules/qlbaiviet/uploads/"> -->
+        
+        <p>Tóm tắt:<?php echo $row_bv['tomtat']?></p>
+        <p>Nội dung:<?php echo $row_bv['noidung']?></p>
+
+    </li>
+   
+</ul>
