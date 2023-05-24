eb630da9e1e8697e7efd2e90e4746b1c5e91f0b9
Hoang <caohoang5969@gmail.com>
2023-05-24 21:32:29 +0700

    index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/stylemain.css">
+    <link rel="stylesheet" type="text/css" href="css/dangky.css">
+    
+    
+    <title>web bán sách</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row"></div>
+            <?php
+                session_start();
+                include('admincp/config/connect.php');
+                include('pages/header.php');
+                include('pages/menu.php');
+                include('pages/main.php');
+                include('pages/footer.php');
+            ?>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
