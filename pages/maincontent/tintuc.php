afa76e28774163e27c6e75d12101bc5b765663d5
Hoang <caohoang5969@gmail.com>
2023-05-24 12:09:44 +0700

    tintuc

---
+++ pages/maincontent/tintuc.php
@@ -0,0 +1,47 @@
+<h3 style="text-align: center;text-transform:uppercase;">Tin tức mới</h3>
+
+<div class="row">
+            <ul class="list_baiviet">
+                
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                <li>
+                    <a href="index.php?quanly=baiviet&id=">
+                    <img src="admincp/modules/qlbaiviet/uploads/">
+                    <p style="word-wrap: break-word;" class="title_product">Tên bài viết: </p>
+                    </a>
+                    <p class="title_product">Tóm tắt:</p>
+                </li>
+                </div>
+                
+            </ul>
+            <div style="clear:both"></div>
+            <style type="text/css">
+                ul.list_trang {
+                    margin: 0;
+                    padding: 0;
+                    list-style: none;
+                }
+                ul.list_trang li {
+                    float: left;
+                    padding: 5px 13px;
+                    margin: 5px;
+                    background: burlywood;
+                    display: block;
+                }
+                ul.list_trang li a{
+                    color: black;
+                    text-align: center;
+                    text-decoration: none;
+                }
+            </style>
+           
+            <p>trang hiện tại:</p>
+            <ul class="list_trang">
+                
+                <li <?php if($i==$page){echo 'style="background: aqua"';} ?> >
+                    <a href="index.php?quanly=tintuc&trang=</a>
+                </li>
+                
+            </ul>
+</div>
+            
