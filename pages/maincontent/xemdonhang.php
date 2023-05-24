e81037fb69a5ec9a8fcaf5b7db8b0f0601d4fa3c
Hoang <caohoang5969@gmail.com>
2023-05-24 12:14:11 +0700

    xemdonhang

---
+++ pages/maincontent/xemdonhang.php
@@ -0,0 +1,27 @@
+<p>xem đơn hàng</p>
+
+<table style="width:100%" border="1" style="border-collapse:collapse;">
+  <tr>
+    <th>Id</th>
+    <th>Mã đơn hàng</th>
+    <th>Tên sản phẩm</th>
+    <th>Số lượng</th>
+    <th>Giá sản phẩm</th>
+    <th>Thành tiền</th>
+  </tr>
+  
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  
+  <tr>
+  <td colspan="6">
+        <p>Tổng tiền: <?php echo number_format($tongtien,0,',','.').'vnđ'?> </p>
+    </td>
+  </tr>
+</table>
