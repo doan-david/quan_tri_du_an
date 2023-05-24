61b1ccc800c1511209036269ef867f5bc1cff94c
dat-nguyen37 <dat958734@gmail.com>
2023-05-24 18:55:48 +0700

    upload

---
+++ admincp/index.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['dangnhap'])){
+        header("Location:login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <title>Admincp</title>
+</head>
+<body>
+    <h3 class="title_admin"> wellcomde to admincp</h3>
+    <div class="wrapper">
+    <?php
+        include('config/connect.php');
+        include('modules/header.php');
+        include('modules/menu.php');
+        include('modules/main.php');
+    ?>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://cdn.ckeditor.com/4.21.0/full/ckeditor.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    <script>
+        CKEDITOR.replace( 'tomtat' );
+        CKEDITOR.replace( 'noidung' );
+        CKEDITOR.replace( 'thongtinlienhe' );
+    </script>
+    <?php
+    ?>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
