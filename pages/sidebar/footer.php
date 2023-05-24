9b78ea2243d882ed1d0cbe99b44d32b18982baf4
Hoang <caohoang5969@gmail.com>
2023-05-24 12:27:37 +0700

    footer

---
+++ pages/sidebar/footer.php
@@ -0,0 +1,72 @@
+
+<div class="clear"></div>
+<!-- footer -->
+<div class="footer">
+    <div class="row">
+        <div class="footer1">
+            <div class="col-md-4">
+            <img class="img img-responsive" width="98%" src="images/bnr.png" alt="">
+            </div>
+            <div class="col-md-4">
+            <img class="img img-responsive" width="98%" src="images/bnr1.png" alt="">
+            </div>
+            <div class="col-md-4">
+            <img class="img img-responsive" width="98%" src="images/bnr2.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="footer2">
+    <table class="row" style="display:flex; padding: 10px">
+    
+    <th class="th">Hỗ trợ khách hàng</th>
+    <th class="th">Về bookstore</th>
+    <th class="th">Hợp tác và liên kết</th>
+    <th class="th">Thông tin liên hệ</th>
+    <tr class="tr">
+            <td>
+                Hotline:1900-1234 <br>
+                Các câu hỏi thường gặp<br>
+                Gửi yêu cầu hỗ trợ<br>
+                Hướng dẫn đặt hàng<br>
+                Phương thức vận chuyển<br>
+                Chính sách đổi trả<br>
+                Hướng dẫn trả gó<br>
+                Hướng dẫn trả góp<br>
+                Chính sách hàng nhập khẩu <br>
+            </td>
+
+            <td>
+                
+                Giới thiệu bookstore <br>
+                Tuyển dụng <br>
+                Chính sách bảo mật thanh toán <br>
+                Chính sách bảo mật thông tin cá nhân <br>
+                Chính sách giải quyết khiếu nại <br>
+                Điều khoản sử dụng <br>
+            </td>
+            
+            <td> <br> 
+                <img class="img img-responsive" width="90%" height="80px" src="images/chungnhan.png" >
+                <br>
+                Quy chế hoạt động Sàn GDTMĐT <br>
+                Bán hàng cùng bookstore <br>
+            </td>
+            
+            <td> <br> 
+                <div>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-sharp fa-solid fa-envelope"></i>
+                <i class="fa-brands fa-apple"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-facebook-messenger"></i>
+                </div>
+
+                Dc:235 Hoàng Quốc Việt, Hà Nội<br>
+                Phone:0978888888 <br>
+                Email:nguyenhuuchien17112k2@gmail.com <br>
+            </td>
+
+    </tr>
+    </table>
+</div>
+</div>
