d2e3fd12b4ab7125323b90915174133fd01418b2
Hoang <caohoang5969@gmail.com>
2023-05-24 11:27:07 +0700

    donhangdadat

---
+++ pages/maincontent/donhangdadat.php
@@ -0,0 +1,8 @@
+<p>dơn hang da dat</p>
+<link rel="stylesheet" type="text/css" href="css/bargiohang.css">
+<div class="wrap">
+    <div class="progress"> <span> <a href="index.php?quanly=giohang" > <i class="fa-solid fa-cart-shopping"></i>Giỏ hàng</a></span> </div>
+    <div class="progress"> <span><a href="index.php?quanly=vanchuyen" > <i class="fa-solid fa-truck-fast"></i>Vận chuyển</a></span> </div>
+    <div class="progress"> <span><a href="index.php?quanly=thongtinthanhtoan" ><i class="fa-solid fa-credit-card"></i>Thanh toán</a><span> </div>
+    <div class="progress"> <span><a href="index.php?quanly=donhangdadat" > <i class="fa-solid fa-clock-rotate-left"></i>Lịch sử đơn hàng</a><span> </div>
+</div>
