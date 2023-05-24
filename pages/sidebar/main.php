31470257b9f4287b4fe429720209328fc3de16e1
Hoang <caohoang5969@gmail.com>
2023-05-24 12:30:04 +0700

    main

---
+++ pages/sidebar/main.php
@@ -0,0 +1,15 @@
+<!-- main -->
+<div class="main">
+    <div class="clear"></div>
+    <div class="row">
+            <!-- large , middle, small, x-small -->
+        <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+            <>
+        </div>
+        <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+            <div class="maincontent">
+                
+            </div>
+        </div>
+    </div>
+</div>
