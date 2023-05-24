688b7e90544508bf64b17cb355ccacf0f0142873
Hoang <caohoang5969@gmail.com>
2023-05-24 12:19:06 +0700

    camon

---
+++ pages/maincontent/camon.php
@@ -0,0 +1,5 @@
+
+<p>Cảm ơn bạn đã mua hàng, chúng tôi sẽ liên hệ với bạn trong thời gian mới nhất</p>
+<p>Mọi thông tin chi tiết xin vui lòng liên hệ</p>
+<p>Số điện thoại: 0978390481</p>
+<p>Email: nguyenhuuchien17112k2@gmail.com</p>
